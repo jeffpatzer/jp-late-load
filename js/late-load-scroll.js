8f618b58ba34a3823f4fb0f7ddc690589134809e
Jeff Patzer <jeff.patzer@moovweb.com>
2011-11-29 22:35:46 -0800

    super simplified script to make sure it works on other pages

--- js/late-load-scroll.js
+++ js/late-load-scroll.js
@@ -1,40 +1,31 @@
-
+// Late load images on scroll
 x$(document).on("DOMContentLoaded", function(){
   var settings = {
     event     :   "scroll",
-    threshold :   300,           container :   window
+    threshold :   300,
+    container :   window
   };
   var convenience_methods = {
-    height: function() {
-      return window.innerHeight;
-    },
-    scrollTop: function() {
-      return window.pageYOffset;
-    },
-    offsetTop: function() {
-      return this[0].offsetTop;
-    },
     belowTheFold: function(settings) {
       if (settings.container === undefined || settings.container === window) {
-        var fold = x$(settings.container).height() + x$(settings.container).scrollTop();
-        }                     return fold <= x$(this).offsetTop() - settings.threshold;    },
-        aboveTheTop: function(settings) {
-          if (settings.container === undefined || settings.container === window) {
-            var fold = x$(window).scrollTop();
-          }
-          return fold >= x$(this).offsetTop() + settings.threshold + x$(this).height();
+        var fold = window.innerHeight + window.pageYOffset;
+      }
+      return fold <= this[0].y - settings.threshold;
+    },
+  }
+  xui.extend(convenience_methods);
+  var imgs = x$("img[data-ll-src]");
+  x$(settings.container).on(settings.event, function(){
+    imgs.each(function() {
+      if (!x$(this).belowTheFold(settings)) {
+        // console.log(this);
+        // console.log(x$(this).offsetTop());
+        if (x$(this).attr('data-ll-loaded') != 'true') {
+          x$(this).attr('data-ll-loaded', 'true');
+          x$(this).attr('src', x$(this).attr('data-ll-src'));
         }
       }
-      xui.extend(convenience_methods);
-      var imgs = x$("img[data-ll-src]");
-      x$(settings.container).on(settings.event, function(){       imgs.each(function() {
-        if (!x$(this).belowTheFold(settings)) {
-          if (x$(this).attr('data-ll-loaded') != 'true') {
-            x$(this).attr('data-ll-loaded', 'true');
-            x$(this).attr('src', x$(this).attr('data-ll-src'));
-          }
-        }
-      });
     });
-    x$(settings.container).fire("scroll");
   });
+  x$(settings.container).fire("scroll");
+});
