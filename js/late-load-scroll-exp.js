d22d4881c21fcbb6e507de3017cffd6c8edc585e
Jeff Patzer <jeff.patzer@moovweb.com>
2011-11-30 09:46:27 -0800

    exposed functions

---
+++ js/late-load-scroll-exp.js
@@ -0,0 +1,34 @@
+// Late load images on scroll
+var mw_lateLoad = function(){
+  var settings = {
+    event     :   "scroll",
+    threshold :   300,
+    container :   window
+  };
+  var convenience_methods = {
+    belowTheFold: function(settings) {
+      if (settings.container === undefined || settings.container === window) {
+        var fold = window.innerHeight + window.pageYOffset;
+      }
+      return fold <= this[0].y - settings.threshold;
+    },
+  }
+  xui.extend(convenience_methods);
+  var imgs = x$("img[data-ll-src]");
+  x$(settings.container).on(settings.event, function(){
+    imgs.each(function() {
+      if (!x$(this).belowTheFold(settings)) {
+        // console.log(this);
+        // console.log(x$(this).offsetTop());
+        if (x$(this).attr('data-ll-loaded') != 'true') {
+          x$(this).attr('data-ll-loaded', 'true');
+          x$(this).attr('src', x$(this).attr('data-ll-src'));
+        }
+      }
+    });
+  });
+};
+x$(document).on("DOMContentLoaded", function() {
+  mw_lateLoad();
+  x$(window).fire("scroll");
+});
