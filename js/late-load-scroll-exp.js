d8643122aa08b49fc4af8c394f19e65e35bda95d
Jeff Patzer <jeff.patzer@moovweb.com>
2011-11-30 09:56:12 -0800

    move functions to a better area

--- js/late-load-scroll-exp.js
+++ js/late-load-scroll-exp.js
@@ -1,19 +1,5 @@
 // Late load images on scroll
 var mw_lateLoad = function(){
-  var settings = {
-    event     :   "scroll",
-    threshold :   300,
-    container :   window
-  };
-  var convenience_methods = {
-    belowTheFold: function(settings) {
-      if (settings.container === undefined || settings.container === window) {
-        var fold = window.innerHeight + window.pageYOffset;
-      }
-      return fold <= this[0].y - settings.threshold;
-    },
-  }
-  xui.extend(convenience_methods);
   var imgs = x$("img[data-ll-src]");
   x$(settings.container).on(settings.event, function(){
     imgs.each(function() {
@@ -29,6 +15,20 @@ var mw_lateLoad = function(){
   });
 };
 x$(document).on("DOMContentLoaded", function() {
+  var settings = {
+    event     :   "scroll",
+    threshold :   300,
+    container :   window
+  };
+  var convenience_methods = {
+    belowTheFold: function(settings) {
+      if (settings.container === undefined || settings.container === window) {
+        var fold = window.innerHeight + window.pageYOffset;
+      }
+      return fold <= this[0].y - settings.threshold;
+    },
+  }
+  xui.extend(convenience_methods);
   mw_lateLoad();
   x$(window).fire("scroll");
 });
