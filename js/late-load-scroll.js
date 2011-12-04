d09daeb78279b3fb0addf1388a31fb510b3621f9
Jeff Patzer <jeff.patzer@moovweb.com>
2011-12-04 14:47:28 -0800

    camelCase :: create more of the object

--- js/late-load-scroll.js
+++ js/late-load-scroll.js
@@ -1,6 +1,7 @@
 // Late load images on scroll
 (function () {
-  function lateload () {
+  function lateLoad() {
+
     this.settings = {
       event     :   "scroll",
       threshold :   300,
@@ -9,8 +10,7 @@
     var settings = this.settings;
     var imgs;
 
-
-    var convenience_methods = {
+    var convenienceMethods = {
       belowTheFold: function() {
         if (settings.container === undefined || settings.container === window) {
           var fold = window.innerHeight + window.pageYOffset;
@@ -18,30 +18,36 @@
         return fold <= this[0].y - settings.threshold;
       },
     }
-    xui.extend(convenience_methods);
+    xui.extend(convenienceMethods);
 
-    this.getImages = function () {
+    this.getImages = function() {
       imgs = x$("img[data-ll-src]")
       return imgs;
     }
-    
-    x$(this.settings.container).on(this.settings.event, function(){
-      imgs.each(function() {
-        if (!x$(this).belowTheFold(this.settings)) {
-          // console.log(this);
-          // console.log(x$(this).offsetTop());
-          if (x$(this).attr('data-ll-loaded') != 'true') {
-            x$(this).attr('data-ll-loaded', 'true');
-            x$(this).attr('src', x$(this).attr('data-ll-src'));
-          }
-        }
-      });
-    });
-
     this.getImages();
-    x$(this.settings.container).fire(this.settings.container.event);
+
+    this.init = function() {
+      x$(this.settings.container).on(this.settings.event, function(){
+        imgs.each(function() {
+          if (!x$(this).belowTheFold(this.settings)) {
+            // console.log(this);
+            // console.log(x$(this).offsetTop());
+            if (x$(this).attr('data-ll-loaded') != 'true') {
+              x$(this).attr('data-ll-loaded', 'true');
+              x$(this).attr('src', x$(this).attr('data-ll-src'));
+            }
+          }
+        });
+      });
+
+      x$(this.settings.container).fire(this.settings.container.event);
+    }
+    this.init();
   }
 
-  document.jpLateload = new lateload;
+  document.jpLateLoad = new lateLoad();
+  // Fire a scroll event to kick the widget off
+  x$(window).fire('scroll');
+
 })();
 
